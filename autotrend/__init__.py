d9313d5b2e7e117837ac675ff716a81430cd197e
CH0KUN <chokunworking4713@gmail.com>
2025-10-10 01:21:22 +0700

    🔨 rearrange plot util

--- autotrend/__init__.py
+++ autotrend/__init__.py
@@ -1,3 +1,36 @@
-from .local_linear_trend import *
-from .plot  import *
-from .gen_data import *
+"""
+AutoTrend: Local Linear Trend Extraction and Visualization
+
+Main exports:
+- decompose_llt: Core LLT decomposition algorithm
+- LLTResult: Result dataclass with trend and prediction marks
+- Plotting functions: plot_error, plot_slope_comparison, plot_full_decomposition, etc.
+- Data generators: generate_simple_wave, generate_behavioral_sequence
+"""
+
+from .local_linear_trend import decompose_llt, LLTResult
+from .plot import (
+    plot_error,
+    plot_slope_comparison,
+    plot_full_decomposition,
+    plot_iteration_grid,
+    plot_model_statistics
+)
+from .gen_data import generate_simple_wave, generate_behavioral_sequence
+
+__all__ = [
+    # Core algorithm
+    'decompose_llt',
+    'LLTResult',
+    
+    # Plotting functions
+    'plot_error',
+    'plot_slope_comparison',
+    'plot_full_decomposition',
+    'plot_iteration_grid',
+    'plot_model_statistics',
+    
+    # Data generators
+    'generate_simple_wave',
+    'generate_behavioral_sequence'
+]
